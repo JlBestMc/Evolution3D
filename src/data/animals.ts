23a0911c5f26bf87258b135e42571d0118868449
JlBestMc <jlrcmt@gmail.com>
2025-09-10 13:53:44 +0200

    animals.ts updated

--- src/data/animals.ts
+++ src/data/animals.ts
@@ -3,8 +3,8 @@ export interface Animal {
   description: string;
   model: string;
   startMa?: number; // millones de años atrás (para orden cronológico)
-  eraId?: string;   // opcional: vincular a una era
-  thumb?: string;   // miniatura opcional
+  eraId?: string; // opcional: vincular a una era
+  thumb?: string; // miniatura opcional
 }
 
 const animals: Animal[] = [
@@ -14,24 +14,29 @@ const animals: Animal[] = [
     model: "/models/whale/Pakicetus3D.glb",
     startMa: 50,
     eraId: "cenozoic",
-    thumb: "/images/cenozoic.png"
+    thumb: "/images/cenozoic.png",
   },
   {
     name: "Archaeopteryx",
     description: "The first bird",
-  model: "/models/chicken/Archaeopteryx3D.glb",
-  startMa: 150,
-  eraId: "mesozoic",
-  thumb: "/images/mesozoic.png"
+    model: "/models/chicken/Archaeopteryx3D.glb",
+    startMa: 150,
+    eraId: "mesozoic",
+    thumb: "/images/mesozoic.png",
   },
   {
     name: "Whale",
     description: "The largest mammal in the world",
-  model: "/models/whale/Whale3D.glb",
-  startMa: 30,
-  eraId: "cenozoic",
-  thumb: "/images/cenozoic.png"
-  }
+    model: "/models/whale/Whale3D.glb",
+    startMa: 30,
+    eraId: "cenozoic",
+    thumb: "/images/cenozoic.png",
+  },
+  {
+    name: "tyrannosaurus rex",
+    description: "One of the largest land carnivores of all time",
+    model: "/models/chicken/tyranosaurus3D.glb",
+  },
 ];
 
 export default animals;
